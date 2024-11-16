5adbdeb6199b679806d3825adbdfbf05b3cd6dad
Dongyeon Min <dongyeon.min@woowahan.com>
2024-11-16 11:40:22 +0900

    [step2] 문자열 계산기 피드백 반영

--- src/main/kotlin/step2/CalculatorProcessor.kt
+++ src/main/kotlin/step2/CalculatorProcessor.kt
@@ -1,27 +1,18 @@
 package step2
 
-internal class CalculatorProcessor {
+class CalculatorProcessor {
     fun process(input: String): Long {
         // validation
         InputValidator.validate(input)
 
-        val iterator = input.split(" ").listIterator()
-        // 첫 번째 숫자를 초기값으로 설정
-        var result = iterator.next().toLong()
+        val tokens = input.split(" ")
+        val initialValue = tokens.first().toLong() // 첫 번째 숫자를 초기값으로 분리
 
-        // iterator 로 연산자와 값을 순차적으로 가져와 계산
-        while (iterator.hasNext()) {
-            val operator = Operator.from(iterator.next())
-            val value = iterator.next().toLong()
-
-            result =
-                when (operator) {
-                    Operator.ADD -> Calculator.add(value1 = result, value2 = value)
-                    Operator.SUBTRACT -> Calculator.subtract(value1 = result, value2 = value)
-                    Operator.MULTIPLY -> Calculator.multiply(value1 = result, value2 = value)
-                    Operator.DIVIDE -> Calculator.divide(numerator = result, denominator = value)
-                }
-        }
-        return result
+        return tokens.drop(1)
+            .chunked(2)
+            .fold(initialValue) { acc, (operator, value) ->
+                val op = Operator.from(operator)
+                op.execute(acc, value.toLong())
+            }
     }
 }
--- src/test/kotlin/step2/CalculatorProcessorTest.kt
+++ src/test/kotlin/step2/CalculatorProcessorTest.kt
@@ -1,8 +1,11 @@
-
 package step2
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
 import io.kotest.matchers.shouldBe
 
 class CalculatorProcessorTest : FunSpec({
@@ -10,118 +13,43 @@ class CalculatorProcessorTest : FunSpec({
 
     context("process() 테스트") {
         context("정상 케이스") {
-            test("숫자가 2개, 연산자가 1개인 경우, 정상적으로 계산이 된다.") {
-                // given
-                val givenInput = "2 + 3"
+            test("여러 입력값을 검증한다.") {
+                val testCases =
+                    table(
+                        headers("input", "expected"),
+                        row("2", 2L),
+                        row("2 + 3", 5L),
+                        row("2 + 3 * 4", 20L),
+                        row("2 + 3 * 4 / 2", 10L),
+                        row("2 + 3 * 4 / 2 - 1", 9L),
+                    )
 
-                // when
-                val result = calculatorProcessor.process(givenInput)
-
-                // then
-                result shouldBe 5
-            }
-            test("숫자가 3개, 연산자가 2개인 경우, 정상적으로 계산이 된다.") {
-                // given
-                val givenInput = "2 + 3 * 4"
-
-                // when
-                val result = calculatorProcessor.process(givenInput)
-
-                // then
-                result shouldBe 20
-            }
-            test("숫자가 4개, 연산자가 3개인 경우, 정상적으로 계산이 된다.") {
-                // given
-                val givenInput = "2 + 3 * 4 / 2"
-
-                // when
-                val result = calculatorProcessor.process(givenInput)
-
-                // then
-                result shouldBe 10
-            }
-            test("(사칙연산을 모두 포함하는 경우) 숫자가 5개, 연산자가 4개인 경우, 정상적으로 계산이 된다.") {
-                // given
-                val givenInput = "2 + 3 * 4 / 2 - 1"
-
-                // when
-                val result = calculatorProcessor.process(givenInput)
-
-                // then
-                result shouldBe 9
+                forAll(testCases) { input, expected ->
+                    calculatorProcessor.process(input) shouldBe expected
+                }
             }
         }
+
         context("실패 케이스") {
-            test("빈 문자열을 입력하는 경우, IllegalArgumentException이 발생한다.") {
-                // given
-                val givenInput = ""
+            test("여러 잘못된 입력값을 검증한다.") {
+                val testCases =
+                    table(
+                        headers("input", "expectedMessage"),
+                        row("", "빈 값이 입력되었습니다. [input: ]"),
+                        row(" 2 + 3", "앞 뒤 공백은 허용되지 않습니다. [input:  2 + 3]"),
+                        row("2 + 3 ", "앞 뒤 공백은 허용되지 않습니다. [input: 2 + 3 ]"),
+                        row("2 + 3 ^ 4", "연산자의 위치에 적절하지 않은 문자가 포함되어 있습니다. [input: 2 + 3 ^ 4]"),
+                        row("2 3 + 4", "연산자의 위치에 적절하지 않은 문자가 포함되어 있습니다. [input: 2 3 + 4]"),
+                        row("2 + 3 * a", "숫자의 위치에 숫자가 아닌 값이 포함되어 있습니다. [input: 2 + 3 * a]"),
+                    )
 
-                // when, then
-                val exception =
-                    shouldThrow<IllegalArgumentException> {
-                        calculatorProcessor.process(givenInput)
-                    }
-                exception.message shouldBe "빈 값이 입력되었습니다."
-            }
-
-            test("입력 문자열이 앞에 공백을 포함하는 경우, IllegalArgumentException이 발생한다.") {
-                // given
-                val givenInput = " 2 + 3"
-
-                // when, then
-                val exception =
-                    shouldThrow<IllegalArgumentException> {
-                        calculatorProcessor.process(givenInput)
-                    }
-                exception.message shouldBe "앞 뒤 공백은 허용되지 않습니다."
-            }
-
-            test("입력 문자열이 뒤에 공백을 포함하는 경우, IllegalArgumentException이 발생한다.") {
-                // given
-                val givenInput = "2 + 3 "
-
-                // when, then
-                val exception =
-                    shouldThrow<IllegalArgumentException> {
-                        calculatorProcessor.process(givenInput)
-                    }
-                exception.message shouldBe "앞 뒤 공백은 허용되지 않습니다."
-            }
-
-            test("입력 문자열의 연산자 위치에 지원하지 않는 연산자가 포함되면, IllegalArgumentException이 발생한다.") {
-                // given
-                val givenInput = "2 + 3 ^ 4"
-
-                // when, then
-                val exception =
-                    shouldThrow<IllegalArgumentException> {
-                        calculatorProcessor.process(givenInput)
-                    }
-                exception.message shouldBe "연산자의 위치에 적절하지 않은 문자가 포함되어 있습니다."
-            }
-
-            test("입력 문자열의 연산자 위치에 숫자가 포함되어있으면, IllegalArgumentException이 발생한다.") {
-                // given
-                val givenInput = "2 3 + 4"
-
-                // when, then
-                val exception =
-                    shouldThrow<IllegalArgumentException> {
-                        calculatorProcessor.process(givenInput)
-                    }
-                exception.message shouldBe "연산자의 위치에 적절하지 않은 문자가 포함되어 있습니다."
-            }
-
-            test("입력 문자열의 숫자 위치에 숫자가 아닌 값이 포함되면, IllegalArgumentException이 발생한다.") {
-                // given
-                val givenInput = "2 + 3 * a"
-
-                // when, then
-                val exception =
-                    shouldThrow<IllegalArgumentException> {
-                        calculatorProcessor.process(givenInput)
-                    }
-                exception.message shouldBe "숫자의 위치에 숫자가 아닌 값이 포함되어 있습니다."
+                forAll(testCases) { input, expectedMessage ->
+                    val exception =
+                        shouldThrow<IllegalArgumentException> {
+                            calculatorProcessor.process(input)
+                        }
+                    exception.message shouldBe expectedMessage
+                }
             }
         }
     }
